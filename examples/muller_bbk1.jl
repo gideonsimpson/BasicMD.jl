c13fd83af12e1674d63bed1eb700bc4263b61da9
Gideon Simpson <gideon.simpson@gmail.com>
2020-04-06 23:51:48 -0400

    tested muller modules

--- examples/muller_bbk1.jl
+++ examples/muller_bbk1.jl
@@ -4,11 +4,12 @@ using Printf
 using Random
 using LinearAlgebra
 using ForwardDiff
-using StaticArrays
 
 push!(LOAD_PATH,"../src/")
 
 using JuBasicMD
+
+include("potentials.jl")
 
 β = 0.1;
 γ = 0.1;
@@ -18,21 +19,7 @@ p₀ = [0.0, 0.0];
 Δt = 1e-2;
 n_iters = 10^6;
 
-function V(x)
-
-    aa = @SVector [-1, -1, -6.5, 0.7];
-    bb = @SVector [0., 0., 11., 0.6];
-    cc = @SVector [-10., -10., -6.5, 0.7];
-    AA = @SVector [-200., -100., -170., 15.];
-    XX = @SVector [1., 0., -0.5, -1.];
-    YY = @SVector [0., 0.5, 1.5, 1.];
-
-    return ( AA[1]*exp(aa[1]*(x[1]-XX[1])^2+bb[1]*(x[1]-XX[1])*(x[2]-YY[1])+cc[1]*(x[2]-YY[1])^2)
-                 +AA[2]*exp(aa[2]*(x[1]-XX[2])^2+bb[2]*(x[1]-XX[2])*(x[2]-YY[2])+cc[2]*(x[2]-YY[2])^2)
-                 +AA[3]*exp(aa[3]*(x[1]-XX[3])^2+bb[3]*(x[1]-XX[3])*(x[2]-YY[3])+cc[3]*(x[2]-YY[3])^2)
-                 +AA[4]*exp(aa[4]*(x[1]-XX[4])^2+bb[4]*(x[1]-XX[4])*(x[2]-YY[4])+cc[4]*(x[2]-YY[4])^2));
-end
-
+V = x->Muller(x);
 cfg = ForwardDiff.GradientConfig(V, q₀);
 gradV! = (gradV, x)-> ForwardDiff.gradient!(gradV, V, x, cfg);
 
--- examples/muller_hmc1.jl
+++ examples/muller_hmc1.jl
@@ -4,11 +4,12 @@ using Printf
 using Random
 using LinearAlgebra
 using ForwardDiff
-using StaticArrays
 
 push!(LOAD_PATH,"../src/")
 
 using JuBasicMD
+
+include("potentials.jl")
 
 β = 0.1;
 M = [1.5, 0.75];
@@ -17,26 +18,11 @@ x₀ = [-0.75, 1.5];
 n_iters = 10^6;
 nΔt = 10^2;
 
-function V(x)
-
-    aa = @SVector [-1, -1, -6.5, 0.7];
-    bb = @SVector [0., 0., 11., 0.6];
-    cc = @SVector [-10., -10., -6.5, 0.7];
-    AA = @SVector [-200., -100., -170., 15.];
-    XX = @SVector [1., 0., -0.5, -1.];
-    YY = @SVector [0., 0.5, 1.5, 1.];
-
-    return ( AA[1]*exp(aa[1]*(x[1]-XX[1])^2+bb[1]*(x[1]-XX[1])*(x[2]-YY[1])+cc[1]*(x[2]-YY[1])^2)
-                 +AA[2]*exp(aa[2]*(x[1]-XX[2])^2+bb[2]*(x[1]-XX[2])*(x[2]-YY[2])+cc[2]*(x[2]-YY[2])^2)
-                 +AA[3]*exp(aa[3]*(x[1]-XX[3])^2+bb[3]*(x[1]-XX[3])*(x[2]-YY[3])+cc[3]*(x[2]-YY[3])^2)
-                 +AA[4]*exp(aa[4]*(x[1]-XX[4])^2+bb[4]*(x[1]-XX[4])*(x[2]-YY[4])+cc[4]*(x[2]-YY[4])^2));
-end
-
+V = x->Muller(x);
 cfg = ForwardDiff.GradientConfig(V, x₀);
 gradV! = (gradV, x)-> ForwardDiff.gradient!(gradV, V, x, cfg);
 
 sampler = HMC(V, gradV!, β, M, Δt, nΔt);
-
 
 Random.seed!(100);
 X₀ = copy(x₀);
@@ -51,9 +37,8 @@ a = avals[end];
 @printf("Mean acceptance rate after %d iterations: %g\n",n_iters, a)
 
 Random.seed!(100);
-X_vals, a_vals = sample_trajectory(x₀, sampler, options=Options(n_iters=n_iters));
-
-histogram2d([X[1] for X in X_vals], [X[2] for X in X_vals],normalize=true,color=:viridis)
+Xvals, avals = sample_trajectory(x₀, sampler, options=Options(n_iters=n_iters));
+histogram2d([X[1] for X in Xvals], [X[2] for X in Xvals],normalize=true,color=:viridis)
 xlims!(-1.5,1.5)
 ylims!(-0.5, 2.0)
 xlabel!("x")
