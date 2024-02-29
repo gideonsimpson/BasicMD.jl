56618b13dea9686edb79ea26809c207244f40318
Gideon Simpson <gideon.simpson@gmail.com>
2024-02-29 13:54:33 -0500

    cleaning up source

--- src/constraints.jl
+++ src/constraints.jl
@@ -1,22 +1,18 @@
-struct Constraints{TA,TB} <: AbstractConstraints
+"""
+Constraints{TA, TB, TI}(before_update!::TA, after_update!::TB, n_before::TI, n_after::TI) where {TA, TB, TI<:Integer}
+
+Set constraints on the sampler, if desired
+### Fields
+* `before_update!` - Constraint to apply before a step of the sampler
+* `after_update!`  - Constraint to apply after a step of the sampler
+* `n_before` - Perform `before!` at `0, n_before-1, 2 n_before-1,...`
+* `n_after` - Perform `after!` at `n_after, 2 n_after,...`
+"""
+struct Constraints{TA,TB,TI<:Integer} <: AbstractConstraints
     before_update!::TA
     after_update!::TB
-    n_before::Int
-    n_after::Int
-    
-    """
-    Constraints{TA,TB}(before!::TA, after!::TB, n_before::TI, n_after::TI) where {TA,TB,TI<:Integer}
-
-    Set constraints on the sampler, if desired
-    ### Fields
-    * `before!` - Constraint to apply before a step of the sampler
-    * `after!`  - Constraint to apply after a step of the sampler
-    * `n_before` - Perform `before!` at `0, n_before-1, 2 n_before-1,...`
-    * `n_after` - Perform `after!` at `n_after, 2 n_after,...`
-    """
-    function Constraints{TA,TB}(before!::TA, after!::TB, n_before::TI, n_after::TI) where {TA, TB, TI<:Integer}
-        return new(before!, after!, n_before, n_after)
-    end
+    n_before::TI
+    n_after::TI
 end
 
 """
