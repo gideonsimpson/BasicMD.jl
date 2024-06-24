15f58b1d8dcfe83fef3a1f44639fd45ac5ff6aa1
Gideon Simpson <gideon.simpson@gmail.com>
2024-06-24 11:28:15 -0400

    revised LM to avoid allocations

--- src/nonmetropolis/first_order/lm.jl
+++ src/nonmetropolis/first_order/lm.jl
@@ -24,6 +24,7 @@ end
 mutable struct LMState{Tx} <: FirstOrderNonMetropolisSamplerState
     x::Tx
     ξ::Tx
+    ξ_new::Tx
     ∇V::Tx
 end
 
@@ -31,24 +32,26 @@ function InitState!(x₀, sampler::LM)
 
     ∇V = similar(x₀);
     ξ = randn(size(x₀));
+    ξ_new = similar(x₀);
     sampler.∇V!(∇V, x₀);
-    return LMState(x₀, ξ, copy(∇V));
+    return LMState(x₀, ξ, ξ_new, copy(∇V))
 end
 
 function InitState(x₀, sampler::LM)
 
     ∇V = similar(x₀);
     sampler.∇V!(∇V, x₀);
-    ξ = randn(size(x₀))
-    return LMState(deepcopy(x₀), ξ, copy(∇V));
+    ξ = randn(size(x₀));
+    ξ_new = similar(x₀)
+    return LMState(deepcopy(x₀), ξ,ξ_new, copy(∇V));
 end
 
 function UpdateState!(state::LMState, sampler::LM)
 
-    ξ_new = randn(size(state.ξ));
+    @. state.ξ_new = randn();
 
-    @. state.x = state.x - sampler.Δt * state.∇V + sampler.σ * .5 *(ξ_new + state.ξ);
-    @. state.ξ = ξ_new;
+    @. state.x = state.x - sampler.Δt * state.∇V + sampler.σ * 0.5 * (state.ξ_new + state.ξ)
+    @. state.ξ = state.ξ_new;
     sampler.∇V!(state.∇V, state.x);
 
     state
